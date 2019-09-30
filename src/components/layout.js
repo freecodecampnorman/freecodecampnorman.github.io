f0b05929dbc29315cf4fc694a77f043f2b820b83
Tse, Christopher <ctse@netsuite.com>
2019-09-30 12:45:07 -0500

    Fix container layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './header';
 import Container from 'react-bootstrap/Container';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './layout.scss';
 
 const Layout = ({ children }) => {
     const data = useStaticQuery(graphql`
@@ -27,22 +29,22 @@ const Layout = ({ children }) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata.title} />
-            <div
+            {/* <div
                 style={{
                     margin: `0 auto`,
                     maxWidth: 960,
                     padding: `0px 1.0875rem 1.45rem`,
                     paddingTop: 0,
                 }}
-            >
-                <main>
-                    <Container>{children}</Container>
-                </main>
-                <footer>
-                    <hr />© {new Date().getFullYear()} freeCodeCamp Norman, Built with{' '}
-                    <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </footer>
-            </div>
+            > */}
+            <Container as="main" id="main">
+                {children}
+            </Container>
+            <footer>
+                <hr />© {new Date().getFullYear()} freeCodeCamp Norman, Built with{' '}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+            {/* </div> */}
         </>
     );
 };
