3d6190ce16c00eafdb4f5d585e40534ab95dc5ee
Tse, Christopher <ctse@netsuite.com>
2019-09-30 13:10:56 -0500

    Fix footer alignment

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,22 +29,17 @@ const Layout = ({ children }) => {
     return (
         <>
             <Header siteTitle={data.site.siteMetadata.title} />
-            {/* <div
-                style={{
-                    margin: `0 auto`,
-                    maxWidth: 960,
-                    padding: `0px 1.0875rem 1.45rem`,
-                    paddingTop: 0,
-                }}
-            > */}
+
             <Container as="main" id="main">
                 {children}
             </Container>
+
             <footer>
-                <hr />© {new Date().getFullYear()} freeCodeCamp Norman, Built with{' '}
-                <a href="https://www.gatsbyjs.org">Gatsby</a>
+                <Container>
+                    <hr />© {new Date().getFullYear()} freeCodeCamp Norman, Built with{' '}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </Container>
             </footer>
-            {/* </div> */}
         </>
     );
 };
