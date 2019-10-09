e3aa77dac1ab955c67a3234ed81368728bff35e4
Tse, Christopher <ctse@netsuite.com>
2019-10-08 23:22:16 -0500

    Add remark transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
                 path: `${__dirname}/src/data`,
             },
         },
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `blog`,
+                path: `${__dirname}/content/blog`,
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
