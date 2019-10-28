26e6e2f8036094198c9a7a207b5f1f17278378a2
OBUAdam <AdamBCollins@gmail.com>
2019-10-28 06:02:29 -0700

    Added instructions for adding a username to the leaderboard.

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -50,6 +50,15 @@ const LeaderboardPage = () => {
         <Layout>
             <SEO title="Leaderboard" />
             <h1>Leaderboard</h1>
+            <h2>Instructions</h2>
+            <ul>
+                <li>Make sure you have an account at <a href="https://www.freecodecamp.org">Freecodecamp.org</a></li>
+                <li>Go to the <a href="https://github.com/freecodecampnorman/freecodecampnorman.github.io">Freecodecamp Norman github</a> and create a fork.
+                <li>In your fork, create a new branch</li>
+                <li>In your new branch, edit the src/data/fccUsers.json file, and add your user name to the bottom (compy and paste someone else's entry and then change the user name to yours)</li>
+                <li>Commit your changes</li>
+                <li>Create a new Pull Request to merge your changes in your branch on your fork into the main repo</li>
+            </ul>
             {loading ? <LoadingIndicator /> : <LeaderboardTable userDataList={userDataList} />}
         </Layout>
     );
